9ad8f98d722e01b996f941cf62b8155275a0b841
JNeubau <neubauer.asia@gmail.com>
2024-06-01 11:41:45 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 source ./environ.sh
 
-echo "Download repository code"
-git clone https://github.com/JNeubau/BD-Flink-NYC-yellow-taxi.git
-
 echo "Download csv from bucket"
 hadoop fs -copyToLocal gs://${INPUT-DATA-LOCATION}
 
