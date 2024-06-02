04e9e872088a5a1fe4e84eabb31e515fdf424d36
JNeubau <neubauer.asia@gmail.com>
2024-06-02 14:00:04 +0200

    kafka zaawansowana cz.1 done

--- TaxiDataAnalysis/src/main/java/com/example/bigdata/TaxiEventsAnalysis.java
+++ TaxiDataAnalysis/src/main/java/com/example/bigdata/TaxiEventsAnalysis.java
@@ -1,18 +1,28 @@
 package com.example.bigdata;
 
 import com.example.bigdata.connectors.TaxiEventSource;
+import com.example.bigdata.model.ResultData;
 import com.example.bigdata.model.TaxiEvent;
 import com.example.bigdata.model.TaxiLocEvent;
+import com.example.bigdata.model.TaxiLocStats;
+import com.example.bigdata.tools.DetInfoProcessWindowFunction;
 import com.example.bigdata.tools.EnrichWithLocData;
+import com.example.bigdata.tools.GetFinalResultWindowFunction;
+import com.example.bigdata.tools.TaxiLocAggregator;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.utils.ParameterTool;
 
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+//import java.sql.Types;
 import java.util.HashMap;
 
 public class TaxiEventsAnalysis {
@@ -34,7 +44,6 @@ public class TaxiEventsAnalysis {
                  Uzupełnij dane w strumieniu o nazwę dzielnicy.
                  Przy okazji pozbądź się danych nieistotnych z punktu widzenia celu przetwarzania
 */
-
         DataStream<TaxiLocEvent> taxiLocEventsDS = taxiEventsDS
                 .map(new EnrichWithLocData(properties.getRequired("zoneFile.path")));
 
@@ -48,14 +57,15 @@ public class TaxiEventsAnalysis {
                  - ile było przyjazdów (startStop = 1)
                  - jaka liczba pasażerów została obsłużona (tylko dla przyjazdów)
                  - jaka sumeryczna kwota została ujszczona za przejazdy (tylko dla przyjazdów)
+*/
 
-        DataStream<ResultData> taxiLocStatsDS = */
+        DataStream<ResultData> taxiLocStatsDS =taxiLocEventsDS
+                .keyBy(TaxiLocEvent::getBorough)
+                .window(EventTimeSessionWindows.withGap(Time.days(1)))
+                .aggregate(new TaxiLocAggregator(), new GetFinalResultWindowFunction());
 
 
-        /* TODO: Podłącz ujście. Wystarczy, że będziesz generował na dane na standardowe wyjście. */
-
-        taxiLocEventsDS.print();
-//        taxiLocStatsDS.print();
+        taxiLocStatsDS.print();
 
         env.execute("Taxi Events Analysis");
     }
