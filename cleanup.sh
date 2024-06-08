752fe1964ee58b2b58ad7b32d73f454343b58b26
JNeubau <neubauer.asia@gmail.com>
2024-06-08 19:27:25 +0200

    ee

--- cleanup.sh
+++ cleanup.sh
@@ -2,7 +2,8 @@ source ./environ.sh
 
 echo "Deleting Kafka topics..."
 kafka-topics.sh --bootstrap-server ${CLUSTER_NAME}-w-1:9092 --delete --topic ${KAFKA_TOPIC_TAXI}
-
+kafka-topics.sh --bootstrap-server ${CLUSTER_NAME}-w-1:9092 --delete --topic ${KAFKA_TOPIC_LOC}
+kafka-topics.sh --bootstrap-server ${CLUSTER_NAME}-w-1:9092 --delete --topic ${ANOMALY_OUTPUT_TOPIC}
 echo 
 
 echo "Delete repository..."
