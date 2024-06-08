afa5c997155a80186504495e353f9bba2ba0a962
JNeubau <neubauer.asia@gmail.com>
2024-06-08 19:36:23 +0200

    ee

--- environ.sh
+++ environ.sh
@@ -6,7 +6,7 @@ export CLUSTER_NAME=$(/usr/share/google/get_metadata_value attributes/dataproc-c
 
 export KAFKA_TOPIC_TAXI="taxi-input-topic"
 export KAFKA_TOPIC_LOC="loc-input-topic"
-export KAFKA_TOPIC_ANOMALY = "anomaly-topic"
+export KAFKA_TOPIC_ANOMALY="anomaly-topic"
 
 export INPUT_DATA_DIR="test"
 export INPUT_DATA_DIR_PROD="yellow-tripdata-result"
