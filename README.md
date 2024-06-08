bbf5bed2a25f83ec3bec68095606cc0e63ec28ce
JNeubau <neubauer.asia@gmail.com>
2024-06-08 19:26:35 +0200

    .

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ gcloud dataproc clusters create ${CLUSTER_NAME} \
 --master-machine-type n1-standard-4 --master-boot-disk-size 50 \
 --num-workers 2 --worker-machine-type n1-standard-2 --worker-boot-disk-size 50 \
 --image-version 2.1-debian11 --optional-components FLINK,DOCKER,ZOOKEEPER \
---project ${PROJECT_ID} --max-age=2h \
+--project ${PROJECT_ID} --max-age=4h \
 --metadata "run-on-master=true" \
 --initialization-actions \
 gs://goog-dataproc-initialization-actions-${REGION}/kafka/kafka.sh
--- create-topics.sh
+++ create-topics.sh
@@ -1,9 +1,12 @@
 source ./environ.sh
 
 echo "Creating topics"
-kafka-topics.sh --bootstrap-server ${CLUSTER_NAME}-w-1:9092 --create \
---replication-factor 1 --partitions 1 --topic ${KAFKA_TOPIC_TAXI}
+kafka-topics.sh --bootstrap-server ${CLUSTER_NAME}-w-1:9092 --create --replication-factor 1 --partitions 1 --topic ${KAFKA_TOPIC_TAXI}
+kafka-topics.sh --bootstrap-server ${CLUSTER_NAME}-w-1:9092 --create --replication-factor 1 --partitions 1 --topic ${KAFKA_TOPIC_LOC}
+kafka-topics.sh --bootstrap-server ${CLUSTER_NAME}-w-1:9092 --create --replication-factor 1 --partitions 1 --topic ${ANOMALY_OUTPUT_TOPIC}
 echo ""
+
+
 
 echo "Available Topics:"
 kafka-topics.sh --bootstrap-server ${CLUSTER_NAME}-w-1:9092 --list
