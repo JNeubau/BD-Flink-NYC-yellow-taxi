1f6b692949e49b82908bfb17d12d30ea44a48559
JNeubau <neubauer.asia@gmail.com>
2024-06-08 21:52:01 +0200

    dupa

--- setup.sh
+++ setup.sh
@@ -64,9 +64,9 @@ docker compose down
 docker compose up -d --wait
 
 echo "Preparing mysql schema"
-docker exec -it mymysql mysql -uroot -ppassword <<< "mysqladmin GRANT ALL ON streamdb.* TO 'user'";
+docker exec -it mymysql mysql -uroot -ppassword <<< "mysqladmin GRANT ALL ON streamdb TO 'user' IDENTIFIED BY 'password' WITH GRANT OPTION";
 
-docker exec -it mymysql mysql --user=user -ppassword -e "CREATE TABLE IF NOT EXISTS taxi_events_sink
+docker exec -it mymysql mysql --user=user -ppassword << "CREATE TABLE IF NOT EXISTS taxi_events_sink
                                     (
                                         borough varchar(255),
                                         from_val varchar(50),
